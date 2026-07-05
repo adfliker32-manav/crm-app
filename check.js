2c25420040ffaf91c1be2129b4f3da1b015a554c
manav <adfliker32@gmail.com>
2026-07-06 01:26:40 +0530

    Cleanup: remove old test and verification scripts

--- check.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-require('mongoose').connect(process.env.MONGO_URI).then(async () => {
-    const msg = await require('./src/models/WhatsAppMessage').findOne({ waMessageId: 'wamid.HBgMOTE5NDI3MTc3NjExFQIAERgSREZBOTgwNUU1MDU4N0I0RURBAA==' });
-    console.log('LATEST_BROADCAST_MSG:', JSON.stringify(msg, null, 2));
-    process.exit(0);
-});
